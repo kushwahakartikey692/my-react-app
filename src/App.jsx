ddb876ea87a5b99f38053eb470bbed8c696d0b73
kushwahakartikey692 <kushwahakartikey692@gmail.com>
2026-09-11 22:17:48 +0530

    Calculator added

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,53 @@
 import { useState } from 'react';
+import './App.css';
+function App() {
+  const [num1, setNum1] = useState(0);
+  const [num2, setNum2] = useState(0);
+  const [result, setResult] = useState(0);
 
-function App(){
-  const [count, setCount] = useState(0);
-  const increment = () =>{
-    setCount(count + 1);
-  };
-  
-return(
-  <>
-<div style={{textAlign: 'center'}}>
-        <h1>Hello World</h1>
-      </div>
-      
-      <div style={{ textAlign: 'center', marginTop: '50px' }}>
-        <p>The count is</p>
-        <h1>{count}</h1>
-        <button onClick={increment}>Add 1</button>
+  function add() {
+    setResult(num1 + num2);
+  }
+
+  function subtract() {
+    setResult(num1 - num2);
+  }
+
+  return (
+    <>
+      <div class='box'>
+        <h1>Simple Calculator</h1>
+        <label>Number 1:</label>
+        <br />
+        <input 
+          type="tel" 
+          value={num1} 
+          onChange={(e) => setNum1(Number(e.target.value))} 
+        />
+        <br />
+        <label>Number 2:</label>
+        <br />
+        <input 
+          type="tel" 
+          value={num2} 
+          onChange={(e) => setNum2(Number(e.target.value))} 
+        />
+        <br />
+        <label>Result</label>
+        <br />
+        <input 
+          type="tel" 
+          placeholder="Result" 
+          value={result} 
+          readOnly 
+        />
+        <br />
+        <button onClick={add}>Add</button>
+        <br />
+        <button onClick={subtract}>Subtract</button>
       </div>
     </>
-);
+  );
 }
 
 export default App;
