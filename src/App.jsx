4efcc340d50a05e67efb9ceb74fb5b07a641faa9
kushwahakartikey692 <kushwahakartikey692@gmail.com>
2026-09-12 10:11:51 +0530

    Updated Calculator

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,51 @@
 import { useState } from 'react';
 import './App.css';
 function App() {
-  const [num1, setNum1] = useState(0);
-  const [num2, setNum2] = useState(0);
-  const [result, setResult] = useState(0);
+  const [num1, setNum1] = useState('');
+  const [num2, setNum2] = useState('');
+  const [result, setResult] = useState('');
+  const [incresult, setIncresult] = useState(0);
 
   function add() {
     setResult(num1 + num2);
   }
+  function reset(){
+    setNum1('')
+    setNum2('')
+    setResult('')
+    setIncresult(0)
+  }
+  function product(){
+    setResult(num1 * num2)
+  }
+
+  function division(){
+    setResult(num1 / num2)
+  }
+
+  function factorial(){
+    let result = 1
+    for(let i = 1; i <= num1; i++){
+        result *= i
+    }
+    setResult(result)
+  }
+
+  function remainder(){
+     setResult(num1 % num2)
+  }
 
   function subtract() {
     setResult(num1 - num2);
+  }
+
+  function increment(){
+    
+    setIncresult(incresult + 1)
+  }
+
+  function decrement(){
+    setIncresult(incresult - 1)
   }
 
   return (
@@ -23,6 +58,7 @@ function App() {
           type="tel" 
           value={num1} 
           onChange={(e) => setNum1(Number(e.target.value))} 
+          placeholder='Enter Nummber'
         />
         <br />
         <label>Number 2:</label>
@@ -31,6 +67,7 @@ function App() {
           type="tel" 
           value={num2} 
           onChange={(e) => setNum2(Number(e.target.value))} 
+          placeholder='Enter Number'
         />
         <br />
         <label>Result</label>
@@ -42,9 +79,30 @@ function App() {
           readOnly 
         />
         <br />
-        <button onClick={add}>Add</button>
+        <label htmlFor="">The count is</label>
         <br />
+        <input 
+        type="tel" 
+        value={incresult}
+        onChange={(e) => setIncresult(Number(e.target.value))} 
+        
+        />
+        <br />
+        <div className='button'>
+        <button onClick={add}>Add</button>
         <button onClick={subtract}>Subtract</button>
+        <br />
+        <button onClick={product}>Product</button>
+        <button onClick={division}>Division</button>
+        <br />
+        <button onClick={remainder}>Remainder</button>
+        <button onClick={factorial}>Factorial</button>
+        <br />
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+        <br />
+        <button onClick={reset}>Reset</button>
+      </div>
       </div>
     </>
   );
